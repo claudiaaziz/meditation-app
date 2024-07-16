8a164aba77d45e9c54f59b65015644fc20a64982
claudiaaziz <claudiahannahh@gmail.com>
2024-07-16 12:48:27 -0400

    Build affirmations index screen

--- app/(tabs)/affirmations.tsx
+++ app/(tabs)/affirmations.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import React from 'react';
+import AppGradient from '@/components/AppGradient';
+import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery';
+import GuidedAffirmationsGallery from '@/components/GuidedAffirmationsGallery';
 
 export default function Affirmations() {
     return (
-        <View>
-            <Text>Affirmations</Text>
+        <View className='flex-1'>
+            <AppGradient colors={['#2E1F58', '#54426B', '#A790AF']}>
+                <ScrollView>
+                    <Text className='text-zinc-50 text-3xl font-bold'>
+                        Change your beliefs with affirmations
+                    </Text>
+                    <View>
+                        {AFFIRMATION_GALLERY.map((category) => (
+                            <GuidedAffirmationsGallery
+                                key={category.title}
+                                title={category.title}
+                                previews={category.data}
+                            />
+                        ))}
+                    </View>
+                </ScrollView>
+            </AppGradient>
         </View>
     );
 }
